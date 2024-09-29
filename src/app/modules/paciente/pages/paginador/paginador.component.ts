a61cb14dd591e29ad9139b6c5f3ff0bf089e3072
Christian Castillo <cyccov@gmail.com>
2024-09-29 17:57:54 -0600

    s

--- src/app/modules/paciente/pages/paginador/paginador.component.ts
+++ src/app/modules/paciente/pages/paginador/paginador.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { PageChangedEvent } from 'ngx-bootstrap/pagination';
+import { IBusquedaPacientes } from '../../interfaces/busqueda/busqueda-pacientes.interface';
 
 @Component({
    selector: 'app-paginador',
@@ -9,8 +10,9 @@ import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 })
 export class PaginadorComponent implements OnInit {
   //TODO: Ver forma de hacer esto para interfaz IBusquedaPacientes
-   contentArray: string[] = new Array(50).fill('');
-   returnedArray!: string[];
+   @Input() contentArray!: IBusquedaPacientes[];
+   ltBus! : IBusquedaPacientes[];
+   returnedArray!: IBusquedaPacientes[];
    showBoundaryLinks: boolean = true;
    showDirectionLinks: boolean = true;
    constructor(private localeService: BsLocaleService) {this.localeService.use('es');}
@@ -21,8 +23,26 @@ export class PaginadorComponent implements OnInit {
       this.returnedArray = this.contentArray.slice(startItem, endItem);
    }
    ngOnInit(): void {
-      this.contentArray = this.contentArray.map((v: string, i: number) => {
-         return 'Line '+ (i + 1);
+      this.contentArray = this.contentArray.map((v: IBusquedaPacientes, i: number) => {
+         return {
+          curp: v.curp,
+          nombres: v.nombres,
+          edad: v.edad,
+          apMaterno:v.apMaterno,
+          apPaterno:v.apPaterno,
+          especialidad:v.especialidad,
+          sexo:v.sexo,
+          domicilio:v.domicilio,
+          localidad:v.localidad,
+          folio:v.folio,
+          fechaNacimiento:v.fechaNacimiento,
+          idPaciente:v.idPaciente,
+          telefono:v.telefono,
+          fechaUltimoExMedico:v.fechaUltimoExMedico,
+          medicoTratante:v.medicoTratante,
+          motivoConsultaOdonto:v.motivoConsultaOdonto,
+          ultimaVisita:v.ultimaVisita
+         };
       });
       this.returnedArray = this.contentArray.slice(0, 5);
    }
